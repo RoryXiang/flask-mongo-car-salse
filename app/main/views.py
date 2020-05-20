9b3ce76521b3dbd74056b6e9a6beb937d1873f30
RoryXiang <pingping1990@gmail.com>
2020-05-20 10:55:54 +0800

    add token and login check function

--- app/main/views.py
+++ app/main/views.py
@@ -10,6 +10,68 @@ from . import main
 from .. import db
 from .. import config
 from ..models import Manager, Sales, Byeer, Cars, Bills
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import functools
+
+
+token_key = "test"
+
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def verify_token(*args, **kwargs):
+        try:
+            # 在请求头上拿到token
+            token = request.headers["z-token"]
+        except Exception:
+            # 没接收的到token,给前端抛出错误
+            # 这里的code推荐写一个文件统一管理。这里为了看着直观就先写死了。
+            return jsonify(code=4103, msg='缺少参数token')
+
+        s = Serializer(current_app.config["SECRET_KEY"])
+        try:
+            s.loads(token)
+        except Exception:
+            return jsonify(code=4101, msg="登录已过期")
+
+        return view_func(*args, **kwargs)
+
+    return verify_token
+
+
+def create_token(user_phone):
+    '''
+    生成token
+    :param api_user:用户id
+    :return: token
+    '''
+
+    # 第一个参数是内部的私钥，这里写在共用的配置信息里了，如果只是测试可以写死
+    # 第二个参数是有效期(秒)
+    s = Serializer(token_key, expires_in=3600)
+    # 接收用户id转换与编码
+    token = s.dumps({"phone": user_phone}).decode("ascii")
+    return token
+
+
+def verify_token(Model, token):
+    '''
+    校验token
+    :param token: 
+    :return: 用户信息 or None
+    '''
+
+    # 参数为私有秘钥，跟上面方法的秘钥保持一致
+    s = Serializer(token_key)
+    try:
+        # 转换为字典
+        data = s.loads(token)
+    except Exception:
+        return None
+    # 拿到转换后的数据，根据模型类去数据库查询用户信息
+    user = Model.objects(phone=data["phone"]).first()
+    return user
+
 
 @main.route("/login", methods=["POST"])
 def login():
@@ -20,32 +82,35 @@ def login():
     print(person.password, "????")
     if not person:
         result_data = {
-            "code": 500,
-            "msg": "帐号密码不匹配",
+            "code": 401,
+            "msg": "帐号不存在",
             "data": {}
         }
     elif md5(login_data["password"].encode()).hexdigest() != person.password:
         result_data = {
-            "code": 500,
+            "code": 401,
             "msg": "帐号密码不匹配",
             "data": {}
         }
     else:
-        session["user"] = person
+        session["user"] = person.phone
+        token = create_token(person.phone)
         result_data = {
             "code": 200,
             "msg": "登录成功",
-            "data": {"data": person}
+            "data": token
         }
     return result_data
 
 
 @main.route("/register", methods=["POST"])
+@login_required
 def register():
     register_data = request.get_data()
     register_data = json.loads(register_data)
     pwd = md5(register_data["password"].encode()).hexdigest()
-    saler = Sales(name=register_data["name"], phone=register_data["phone"], email="", password=pwd, creater=register_data["creater"])
+    saler = Sales(name=register_data["name"], phone=register_data["phone"],
+                  email="", password=pwd, creater=register_data["creater"])
     try:
         saler.save()
         data = {
