bc2284fa45c2097e33e914d0139170959fb9ad92
rory-lenove <pingping19901121@gmail.com>
2020-05-21 15:39:02 +0800

    not use class inherit

--- app/models.py
+++ app/models.py
@@ -19,9 +19,20 @@ class BasePerson(mongo.Document):
     meta = {
         "allow_inheritance": True
     }
+    # 如果用类继承的话在数据库，都是存在baseperson这个集合里面，只是用_cls字段（基础类名.继承雷鸣：BasePerson.Manager)来区分
 
 
-class Manager(BasePerson):
+class Manager(mongo.Document):
+    id = mongo.SequenceField(primary_key=True)
+    name = mongo.StringField()
+    phone = mongo.StringField(required=True, max_length=11, unique=True)
+    email = mongo.StringField()
+    password = mongo.StringField(required=True)
+    create_tiem = mongo.DateTimeField(default=datetime.datetime.now, required=True)
+    ismaster = mongo.BooleanField()
+    education = mongo.StringField()
+    self_introduction = mongo.StringField()
+    creater = mongo.StringField(required=True)
     meta = {
         "collection": "manager",
         "index": [{
@@ -31,7 +42,16 @@ class Manager(BasePerson):
     }
 
 
-class Sales(BasePerson):
+class Sales(mongo.Document):
+    id = mongo.SequenceField(primary_key=True)
+    name = mongo.StringField()
+    phone = mongo.StringField(required=True, max_length=11, unique=True)
+    email = mongo.StringField()
+    password = mongo.StringField(required=True)
+    create_tiem = mongo.DateTimeField(default=datetime.datetime.now, required=True)
+    ismaster = mongo.BooleanField()
+    education = mongo.StringField()
+    self_introduction = mongo.StringField()
     master_belong = mongo.IntField()
     creater = mongo.ReferenceField(Manager)
     meta = {
@@ -43,7 +63,16 @@ class Sales(BasePerson):
     }
 
 
-class Byeer(BasePerson):
+class Byeer(mongo.Document):
+    id = mongo.SequenceField(primary_key=True)
+    name = mongo.StringField()
+    phone = mongo.StringField(required=True, max_length=11, unique=True)
+    email = mongo.StringField()
+    password = mongo.StringField(required=True)
+    create_tiem = mongo.DateTimeField(default=datetime.datetime.now, required=True)
+    ismaster = mongo.BooleanField()
+    education = mongo.StringField()
+    self_introduction = mongo.StringField()
     meta = {
         "collection": "byeer",
         "index": [{
@@ -55,6 +84,7 @@ class Byeer(BasePerson):
 
 class Cars(mongo.Document):
     id = mongo.SequenceField(primary_key=True)  # 让_id自增
+    insert_manager = mongo.ReferenceField(Manager)
     name = mongo.StringField(required=True, unique=True)
     min_price = mongo.FloatField(required=True)
     max_price = mongo.FloatField(required=True)
